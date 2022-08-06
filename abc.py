5a9e6b54811cad7c624fafa543da9b63fb2b95bb
male <1615316921@qq.com>
2022-08-07 04:58:04 +0800

    2022-08-07 04:56:00 ide pycharm 第一次提交

---
+++ abc.py
@@ -0,0 +1,8 @@
+# encoding='utf8'
+# file->settings->Editor->file and Code Templates 
+# -> Python Script 输入自己的模板
+# 当前时间 2022/8/7 4:55
+
+print('hello world!')
+print('ide第一次提交')
+print('2022-08-07 04:56:00')
